fbc9120267563c03bf59c6478257adab5d357bbe
Paulo Salvatore <salvatoregames@gmail.com>
2024-04-18 23:51:28 +0000

    Criação do Card com estilização (CSS) e estrutura (HTML)

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import './App.css'
 function App() {
   return (
     <>
-      Hello, world!
+      <div className="card">
+        <h2>Rick Sanchez</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+      </div>
     </>
   )
 }
