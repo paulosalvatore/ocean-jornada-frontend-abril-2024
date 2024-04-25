06d533c81279c9380f2372fd49a763b042230f76
Paulo Salvatore <salvatoregames@gmail.com>
2024-04-25 21:30:21 +0000

    Desafio 1 resolvido

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ function App() {
     <>
       <div className="card">
         <h2>Rick Sanchez</h2>
+
+        <div className="tags">
+          <div>Status: Vivo</div>
+          <div>Espécie: Humana</div>
+          <div>Origem: Terra C-137</div>
+        </div>
+
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
       </div>
     </>
