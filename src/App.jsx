7a43a8b8a242d6b9af916781c902bb45e0db0b19
Paulo Salvatore <salvatoregames@gmail.com>
2024-04-25 21:47:43 +0000

    Criação do componente `<Card />`

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
   return (
     <>
-      <div className="card">
-        <h2>Rick Sanchez</h2>
-
-        <div className="tags">
-          <div>Status: Vivo</div>
-          <div>Espécie: Humana</div>
-          <div>Origem: Terra C-137</div>
-        </div>
-
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-      </div>
+      <Card />
+      <Card />
+      <Card />
     </>
   )
 }
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,15 @@
+export default function Card() {
+    return (
+        <div className="card">
+            <h2>Rick Sanchez</h2>
+
+            <div className="tags">
+            <div>Status: Vivo</div>
+            <div>Espécie: Humana</div>
+            <div>Origem: Terra C-137</div>
+            </div>
+
+            <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+        </div>
+    )
+}
