98eef9617c00f61dbd4ed1241451e8a80d7f37bc
Paulo Salvatore <salvatoregames@gmail.com>
2024-04-25 22:18:37 +0000

    Implementação do `map` para carregar lista de `<Card />`

--- src/App.jsx
+++ src/App.jsx
@@ -17,11 +17,13 @@ function App() {
     image: 'https://rickandmortyapi.com/api/character/avatar/3.jpeg'
   }
 
+  const itens = [item1, item2, item3]
+
   return (
     <>
-      <Card item={item1} />
-      <Card item={item2} />
-      <Card item={item3} />
+      {itens.map(function (elemento) {
+        return <Card item={elemento} />
+      })}
     </>
   )
 }
