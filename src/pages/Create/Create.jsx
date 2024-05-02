3a24cc1b377545ee1874981cfc51d643d6dd9854
Paulo Salvatore <salvatoregames@gmail.com>
2024-05-02 22:15:23 +0000

    Conteúdo JSX da página `Create`

--- src/pages/Create/Create.jsx
+++ src/pages/Create/Create.jsx
@@ -1,3 +1,22 @@
+import { Link } from "react-router-dom";
+
 export default function Create() {
-  return <div>Create</div>
+  return (
+    <>
+      <Link to="/">Home</Link>
+      <div>
+        <form>
+            <label htmlFor="name">Nome:</label>
+            <input type="text" id="name" name="name" placeholder="Digite o nome" />
+            <br />
+
+            <label htmlFor="image">Imagem:</label>
+            <input type="text" id="image" name="image" placeholder="Digite a URL da imagem" />
+            <br />
+
+            <input type="submit" value="Criar" />
+        </form>
+      </div>
+    </>
+  )
 }
