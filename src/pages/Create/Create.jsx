dc024bd9d2f32c463af495f016f38a5f1bc52826
Paulo Salvatore <salvatoregames@gmail.com>
2024-05-02 23:29:00 +0000

    Enviando a requisição POST para criação da personagem

--- src/pages/Create/Create.jsx
+++ src/pages/Create/Create.jsx
@@ -1,22 +1,39 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function Create() {
-  function onSubmit(event) {
+  const navigate = useNavigate()
+
+  async function onSubmit(event) {
+    // Previne que o form saia da página
     event.preventDefault()
 
+    // Pega os dados de name e image dos inputs
     const name = event.target.name.value
     const image = event.target.image.value
 
+    // Monta um objeto com os dados da personagem
     const data = {
       name,
       image
     }
 
+    // Transforma um objeto em uma string JSON
     const json = JSON.stringify(data)
 
-    console.log(data, json)
+    // Declara a URL da API para realização do POST
+    const apiUrl = 'https://ocean-jornada-backend-abril-2024.onrender.com/item'
 
-    // TODO: Implementar requisição na API, enviando o JSON
+    // Cria e executa a requisição HTTP para o endpoint POST
+    const response = await fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: json
+    })
+
+    // Navega para a página principal
+    return navigate('/')
   }
 
   return (
